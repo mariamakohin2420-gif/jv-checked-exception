425e26fc7a7e0bdf71abc990f8ecfae307353148
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-22 14:40:00 +0200

    Second changes

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public class PaswordValidationException extends Exception {
+public class PasswordValidationException extends Exception {
     public PasswordValidationException(String message) {
         super(message);
     }
